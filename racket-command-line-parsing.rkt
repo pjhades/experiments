598ef920c9ab69c61c37de09019c4a55dbf3911f
Jing Peng <pj.hades@gmail.com>
2017-04-23 11:38:47 -0400

    Update racket command line parsing example

--- racket-command-line-parsing.rkt
+++ racket-command-line-parsing.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require racket/cmdline)
+(require racket/match)
 
 (define cur-filename (path->string (find-system-path 'run-file)))
 
@@ -10,41 +11,6 @@
                             (cherry . 0) (donut . 0)
                             ; mutually exclusive
                             (egg . 0) (fennel . 0))))
-
-(define (+x str) (lambda (base) (+ base (string->number str))))
-
-(printf "command line arguments: ~a~n" (current-command-line-arguments))
-(command-line #:program cur-filename
-              #:multi
-              [("-a" "--apple") apple-arg
-                                "Specify the number of apples"
-                                (hash-update! things 'apple (+x apple-arg))]
-              [("-b" "--banana") banana-arg
-                                 "Specify the number of bananas"
-                                 (hash-update! things 'banana (+x banana-arg))]
-              #:once-each
-              [("-c" "--cherry") cherry-arg
-                                 "Specify the number of cherries"
-                                 (hash-set! things 'cherry cherry-arg)]
-              [("-d" "--donut") donut-arg
-                                "Specify the number of donuts"
-                                (hash-set! things 'donut donut-arg)]
-              #:once-any
-              [("-e" "--egg") egg-arg
-                              "Specify the number of eggs"
-                              (hash-set! things 'egg egg-arg)]
-              [("-f" "--fennel") fennel-arg
-                                 "Specify the number of fennels"
-                                 (hash-set! things 'fennel fennel-arg)]
-              #:usage-help
-              "Come on this is actually of no use."
-              "And you need to believe me."
-              #:help-labels
-              "This is the description of help labels."
-              "Apparently another useless part."
-              #:args
-              args
-              (printf "Non-option arguments: ~a~n" args))
 
 (define (print-things)
   (define (iter pos)
@@ -57,5 +23,63 @@
   (let ([pos (hash-iterate-first things)])
     (printf "-----------~n")
     (iter pos)))
+  
+(define (parse-cmd [argv (current-command-line-arguments)])
+  (define (+x str) (lambda (base) (+ base (string->number str))))
 
-(print-things)
+  (printf "command line arguments: ~a~n" (current-command-line-arguments))
+  (command-line #:program cur-filename
+                #:argv argv
+                #:multi
+                [("-a" "--apple") apple-arg
+                                  "Specify the number of apples"
+                                  (hash-update! things 'apple (+x apple-arg))]
+                [("-b" "--banana") banana-arg
+                                   "Specify the number of bananas"
+                                   (hash-update! things 'banana (+x banana-arg))]
+                #:once-each
+                [("-c" "--cherry") cherry-arg
+                                   "Specify the number of cherries"
+                                   (hash-set! things 'cherry cherry-arg)]
+                [("-d" "--donut") donut-arg
+                                  "Specify the number of donuts"
+                                  (hash-set! things 'donut donut-arg)]
+                #:once-any
+                [("-e" "--egg") egg-arg
+                                "Specify the number of eggs"
+                                (hash-set! things 'egg egg-arg)]
+                [("-f" "--fennel") fennel-arg
+                                   "Specify the number of fennels"
+                                   (hash-set! things 'fennel fennel-arg)]
+                #:usage-help
+                "Come on this is actually of no use."
+                "And you need to believe me."
+                #:help-labels
+                "This is the description of help labels."
+                "Apparently another useless part."
+                #:args
+                args
+                (printf "Non-option arguments: ~a~n" args)))
+
+(define (normal)
+  (parse-cmd)
+  (print-things))
+
+(define (git-like)
+  (let* ([argv (current-command-line-arguments)]
+         [first-arg (vector-ref argv 0)]
+         [rest-args (vector-drop argv 1)])
+    (match first-arg
+      ["init" "initialize"]
+      ["branch" "branch operations"]
+      ["commit" (parse-cmd rest-args)
+                (print-things)]
+      ["push" "push commits to remote"]
+      [_ "what?!"])))
+
+; ./xxx -a 10 -b 90
+;(normal)
+
+; ./xxx commit -a 10 -b 90
+(git-like)
+
