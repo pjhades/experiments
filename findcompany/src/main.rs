e093b6cf3b5af38c69eaf573168ce02c91144db4
Jing Peng <pj.hades@gmail.com>
2019-01-16 22:40:18 -0500

    Add simple pagination

--- findcompany/src/main.rs
+++ findcompany/src/main.rs
@@ -1,44 +1,82 @@
-extern crate reqwest;
+extern crate regex;
 extern crate json;
+extern crate reqwest;
 
-use reqwest::{header, Client, Url};
+use reqwest::{Client, Response, Url};
+use reqwest::header::{AUTHORIZATION, LINK};
+use json::JsonValue;
+use regex::Regex;
 
-fn main() {
-    let token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-    let location = "montreal";
-    let client = Client::new();
-    let mut resp = client.get("https://api.github.com/search/users")
-        .query(&[("q", format!("location:{}", location).as_str())])
-        .header(header::AUTHORIZATION, format!("token {}", token).as_str())
-        .send()
-        .unwrap();
+const API: &str = "https://api.github.com/";
 
-    let j = json::parse(resp.text().unwrap().as_str()).unwrap();
+struct Q {
+    client: Client,
+    token: String,
+    cur_page: usize,
+    total_page: usize,
+}
 
-    if !j.has_key("items") {
-        panic!("Request failed. Errors:\n{}", j.pretty(2));
+fn parse_json(mut resp: Response) -> JsonValue {
+    json::parse(resp.text().unwrap().as_str()).unwrap()
+}
+
+impl Q {
+    fn query(&self, url: Url, params: Option<&[(&str, &str)]>) -> Response {
+        let mut builder = self.client.get(url);
+        if let Some(p) = params {
+            builder = builder.query(p);
+        }
+        builder.header(AUTHORIZATION, format!("token {}", self.token).as_str()).send().unwrap()
     }
 
-    let total_count = &j["total_count"];
-    let incomplete = &j["incomplete_results"];
-    println!("count={} incomplete={}", total_count, incomplete);
+    fn users_at_location(&mut self, location: &str) -> JsonValue {
+        self.cur_page += 1;
 
-    for user in j["items"].members() {
-        let url = Url::parse("https://api.github.com/users/")
-            .unwrap()
-            .join(&user["login"].as_str().unwrap())
-            .unwrap();
-        let mut r = client.get(url)
-            .header(header::AUTHORIZATION, format!("token {}", token).as_str())
-            .send()
-            .unwrap();
-        let u = json::parse(r.text().unwrap().as_str()).unwrap();
-        u.pretty(2);
-        if u.has_key("company") {
-            let company = &u["company"];
+        let url = Url::parse(API).unwrap().join("search/users").unwrap();
+        let resp = self.query(url, Some(&[
+            ("q", format!("location:{}", location).as_str()),
+            ("per_page", "100"),
+            ("page", format!("{}", self.cur_page).as_str()),
+        ]));
+
+        let re = Regex::new(r#"page=(\d+)>; rel="last""#).unwrap();
+        let text = resp.headers().get(LINK).unwrap().to_str().unwrap();
+        let caps = re.captures(text).unwrap();
+        self.total_page = caps.get(1).unwrap().as_str().parse().unwrap();
+        println!("{}/{} pages", self.cur_page, self.total_page);
+
+        let mut j = parse_json(resp);
+        if !j.has_key("items") {
+            panic!("Request failed. Returned JSON:\n{}", j.pretty(2));
+        }
+        j["items"].take()
+    }
+
+    fn user_profile(&self, username: &str) -> JsonValue {
+        let url = Url::parse(API).unwrap().join("users/").unwrap().join(username).unwrap();
+        parse_json(self.query(url, None))
+    }
+}
+
+fn main() {
+    let mut q = Q {
+        client: Client::new(),
+        token: "x".to_string(),
+        total_page: 0,
+        cur_page: 0,
+    };
+
+    loop {
+        for user in q.users_at_location("montreal").members() {
+            let profile = q.user_profile(user["login"].as_str().unwrap());
+            let company = &profile["company"];
             if !company.is_null() {
-                println!("{}", company)
+                println!("{:-20} {:-20} {}", profile["login"], company, profile["url"]);
             }
+        }
+
+        if q.cur_page == q.total_page {
+            break;
         }
     }
 }
