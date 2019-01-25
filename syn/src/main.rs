4f35c74ba8943833e6eb8edaee968612d05856b6
Jing Peng <pj.hades@gmail.com>
2019-01-25 16:41:11 -0500

    WIP

--- syn/src/main.rs
+++ syn/src/main.rs
@@ -15,6 +15,10 @@ impl S {
         // not work properly
         match expr {
             syn::Expr::Call(ref expr_call) => println!("call: {:?}", expr_call.func),
+            // consider chained method calls:
+            // ptr.func1().func2().func3()
+            // look into the ExprMethodCall.receiver field, where
+            // there may be a chain of calls
             syn::Expr::MethodCall(ref method_call) => println!("method call: {:?}", method_call.method),
             _ => (),
         }
@@ -27,9 +31,11 @@ impl<'ast> Visit<'ast> for S {
         for stmt in f.block.stmts.iter() {
             // need recursively search for function calls in all possible stmt types
             match stmt {
+                syn::Stmt::Local(ref local) => 
+                syn::Stmt::Item(ref item) |
                 syn::Stmt::Expr(ref expr) |
                 syn::Stmt::Semi(ref expr, _) => {
-                    println!("expr: {:#?}", expr);
+                    //println!("expr: {:#?}", expr);
                     self.find_fn_calls(expr)
                 }
                 s @ _ => println!("whatever: {:#?}", s),
