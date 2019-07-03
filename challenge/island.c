51cf90a0dd188dcdd6418ced0dac6ed61d9229ef
Jing Peng <pj.hades@gmail.com>
2019-07-02 22:21:26 -0400

    Add island finding solution

---
+++ challenge/island.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#define N 1000
+#define M 1000
+
+int uf[N*M];
+int n, m;
+char map[N][M];
+int dir[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+void u(int x, int y) {
+    if (x == y)
+        return;
+    if (uf[x] < uf[y]) {
+        uf[x] += uf[y];
+        uf[y] = x;
+    }
+    else {
+        uf[y] += uf[x];
+        uf[x] = y;
+    }
+}
+
+int f(int x) {
+    int i;
+    for (i = x; uf[i] >= 0; i = uf[i]);
+    return i;
+}
+
+int main() {
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n; i++) {
+        scanf("%s", &map[i]);
+        for (int j = 0; j < m; j++)
+            uf[i*m + j] = -1;
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (map[i][j] == '0')
+                continue;
+            for (int k = 0; k < 4; k++) {
+                int ni = i + dir[k][0], nj = j + dir[k][1];
+                if (ni < 0 || ni >= n || nj < 0 || nj >= m || map[ni][nj] == '0')
+                    continue;
+                u(f(i*m+j), f(ni*m+nj));
+            }
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+        if (map[i][j] == '1' && uf[i*m + j] < 0)
+            ans++;
+    printf("%d\n", ans);
+    return 0;
+}
